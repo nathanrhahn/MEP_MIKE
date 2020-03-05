b1f244500172c2bac52eacc576ddffd4a9b0d004
nathanrhahn <nhahn@colostate.edu>
2020-03-05 14:13:15 -0700

    Updated TODO list

--- mike_main.R
+++ mike_main.R
@@ -5,8 +5,9 @@ mike <- read.csv("C:/Users/nhahn/Dropbox (Personal)/MEP_Working/Data/ER_Events/e
 mike$event_details_CauseOfDeath <- as.factor(mike$event_details_CauseOfDeath)
 
 #TODO: create seperate cause and means columns
-#TODO: Correlation between cause and means
-#TODO: What 
+#TODO: correlation between cause and means
+#TODO: create cause-specific map 
+#TODO: ratio of conflict events to conflict killings within each MEP HWC sector
 
 mike$cause
 mike$means 
