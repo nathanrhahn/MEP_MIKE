85d47b134bf758e8e8635d8b3c1075e84de0bcdf
nathanrhahn <nhahn@colostate.edu>
2020-03-05 14:37:07 -0700

    Split CauseOfDeath into cause and means

--- mike_main.R
+++ mike_main.R
@@ -1,16 +1,20 @@
 ## MIKE Summary Stats
 
-mike <- read.csv("C:/Users/nhahn/Dropbox (Personal)/MEP_Working/Data/ER_Events/events_csv/MEP-MIKE.csv")
+library(tidyverse)
 
-mike$event_details_CauseOfDeath <- as.factor(mike$event_details_CauseOfDeath)
-
-#TODO: create seperate cause and means columns
-#TODO: correlation between cause and means
+#TODO: Chi-Squared between cause and means
 #TODO: create cause-specific map 
 #TODO: ratio of conflict events to conflict killings within each MEP HWC sector
 
-mike$cause
-mike$means 
+
+mike <- read.csv("C:/Users/nhahn/Dropbox (Personal)/MEP_Working/Data/ER_Events/events_csv/MEP-MIKE.csv")
+
+
+# split CauseOfDeath into cause and means
+mike <- mutate(mike, temp = str_replace(event_details_CauseOfDeath, "_", "-")) 
+mike <- separate(mike,temp, c("cause", "means"), sep = "-", remove = TRUE)
+
+
 
 
 
